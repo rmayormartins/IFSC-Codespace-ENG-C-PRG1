ee15d63ac5c35450e6e1732925850a397f37d264
Ramon Mayor Martins <rmayormartins@gmail.com>
2024-11-08 18:08:29 +0000

    go

---
+++ teste2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main()
+{
+    float valor1;
+
+    printf("Entre com o Valor1:\n");
+    scanf("%f", &valor1);
+    printf("Valor eh %.2f\n", valor1);
+
+    return 0;
+}
